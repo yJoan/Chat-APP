fc98cea86641a54b010759878dba811b69c8f745
joan <488677134@qq.com>
2019-12-02 21:18:32 +0800

    上传基线代码

---
+++ MFCChatClinet/resource.h
@@ -0,0 +1,32 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCChatClinet.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCCHATCLINET_DIALOG        102
+#define IDR_MAINFRAME                   128
+#define IDC_LIST1                       1001
+#define IDC_EDIT1                       1002
+#define IDC_PORT_EDIT                   1003
+#define IDC_SENDMSG_EDIT                1004
+#define IDC_AUTOSENDMSG_EDIT            1005
+#define IDC_COLOUR_COMBO                1006
+#define IDC_CONNECTBTN_BUTTON           1007
+#define IDC_AUTOSEND_RADIO1             1008
+#define IDC_IPADDRESS                   1009
+#define IDC_DISCONNECT_BTN              1010
+#define IDC_CLEARMSG_BTN                1011
+#define IDC_SEND_BTN                    1012
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
---
+++ MFCChatServer/MFCChatServerDlg.h
@@ -0,0 +1,51 @@
+﻿
+// MFCChatServerDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CMFCChatServerDlg 对话框
+class CMFCChatServerDlg : public CDialogEx
+{
+// 构造
+public:
+	CMFCChatServerDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCCHATSERVER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedButton3();
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedButton5();
+	afx_msg void OnEnChangeEdit2();
+	afx_msg void OnLbnSelchangeMsgList();
+	afx_msg void OnEnChangePortEdit();
+	afx_msg void OnEnChangeSendEdit();
+	afx_msg void OnCbnSelchangeCombo2();
+	afx_msg void OnCbnSelchangeColourCombo();
+	afx_msg void OnBnClickedCalBtn();
+	afx_msg void OnBnClickedCleaarBtn();
+	afx_msg void OnBnClickedStopBtn();
+	afx_msg void OnBnClickedMailBtn();
+	afx_msg void OnBnClickedStartBtn();
+};
